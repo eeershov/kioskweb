acd90d2d9ceab6629fd53affd48595748d408f28
Evgeniy Ershov <ershov.ee@gmail.com>
2023-05-11 19:28:29 +0300

    MonthView ability to switch months

--- client/src/components/Calendar/Calendar.tsx
+++ client/src/components/Calendar/Calendar.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { ApiService } from '../../services/api.service';
-import { setDefaultOptions } from "date-fns";
+import { setDefaultOptions, format } from "date-fns";
 import { ru } from 'date-fns/locale';
 import { WeekView, MonthView } from "./index";
 import { ViewportContext } from '../../appContext/ViewportContext';
@@ -13,14 +13,19 @@ setDefaultOptions({ locale: ru, weekStartsOn: 1 });
 function Calendar() {
   console.log("Calendar");
   const mobOrDesk = useContext(ViewportContext);
+  const todayDate = new Date();
 
+  const [selectedDate, setSelectedDate] = useState(todayDate);
   const [eventsByDay, setEventsByDay] = useState<Map<string, [] | EventWithOrganizationData[]>>(new Map());
 
   useEffect(() => {
     let active = true;
     async function fetchData() {
-      ApiService.getCalendarData().then(response => {
+      const dateFormat = `yyyy-MM-dd`;
+      const selectedDateString = format(selectedDate, dateFormat);
+      ApiService.getCalendarData(selectedDateString).then(response => {
         if (active) {
+          console.log(response);
           setEventsByDay(response);
         }
       });
@@ -29,9 +34,10 @@ function Calendar() {
     return () => {
       active = false;
     };
-  }, []);
+  }, [selectedDate]);
 
-  const MonthOrWeekView = (mobOrDesk === "Mobile" ? <WeekView eventsByDay={eventsByDay} /> : <MonthView eventsByDay={eventsByDay} />)
+  const MonthOrWeekView = (mobOrDesk === "Mobile" ? <WeekView eventsByDay={eventsByDay} selectedDate={selectedDate} todayDate={todayDate} /> :
+    <MonthView eventsByDay={eventsByDay} setSelectedDate={setSelectedDate} todayDate={todayDate} selectedDate={selectedDate} />)
 
 
   return (
--- client/src/components/Calendar/MonthView.tsx
+++ client/src/components/Calendar/MonthView.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { setDefaultOptions, parse, getDay } from "date-fns";
+import React, { Dispatch, SetStateAction } from 'react';
+import { setDefaultOptions, parse, getDay, subDays, addDays } from "date-fns";
 import { ru } from 'date-fns/locale';
 import { Week } from "./index";
 import { getWeekEvents } from "./utils";
@@ -8,11 +8,15 @@ import type { EventWithOrganizationData } from "../../types/EventWithOrg.type";
 
 setDefaultOptions({ locale: ru, weekStartsOn: 1 });
 
-interface EventsByDay {
-  eventsByDay: Map<string, EventWithOrganizationData[] | []>
+interface Props {
+  eventsByDay: Map<string, EventWithOrganizationData[] | []>,
+  selectedDate: Date,
+  setSelectedDate: Dispatch<SetStateAction<Date>>,
+  todayDate: Date
 }
 
-export default function MonthView({ eventsByDay }: EventsByDay) {
+
+export default function MonthView({ eventsByDay, selectedDate, setSelectedDate, todayDate }: Props) {
   console.log("MonthView");
   const dateFormat = `d-M-yyyy`;
   const currentDate = new Date();
@@ -42,11 +46,43 @@ export default function MonthView({ eventsByDay }: EventsByDay) {
     weeksJSX.push(<Week key={i} weekEvents={getWeekEvents(date, eventsByDay)} isWeekdayEmpty={weekInfo.isWeekdayEmpty} />)
   }
 
+  function handleClick(option: "prev" | "next") {
+    if (option === "prev") {
+      setSelectedDate(
+        subDays(selectedDate, 31)
+      )
+    } else {
+      setSelectedDate(
+        addDays(selectedDate, 31)
+      )
+    }
+  }
+
+  function ControlButton({ option, children }: { option: "prev" | "next"; children: string }): JSX.Element {
+    console.log(option);
+    let status = false;
+    // if (option === "prev") {
+    //   status = isDisabledControls.prev;
+    // } else {
+    //   status = isDisabledControls.next;
+    // }
+    return (
+      <button onClick={() => handleClick(option)} disabled={status} className='bg-slate-500 disabled:bg-stone-500'>
+        {children}
+      </button>
+    );
+  }
 
   return (
     <div className='WeekView
                     flex-row m-0'>
       <p>MonthView!</p>
+      <ControlButton option='prev'>
+        Предыдущий месяц
+      </ControlButton>
+      <ControlButton option='next'>
+        Следующий месяц
+      </ControlButton>
       <div className='Week flex'>
         <div className='basis-2/12'>ПН</div>
         <div className='basis-2/12'>ВТ</div>
