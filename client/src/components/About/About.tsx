4ba023cff2c678ad5bae7f4dadc87c885ff063e2
Evgeniy Ershov <ershov.ee@gmail.com>
2023-05-30 11:47:20 +0300

    About and Socials decoupling

--- client/src/components/About/About.tsx
+++ client/src/components/About/About.tsx
@@ -1,20 +1,6 @@
 import React from "react";
 import photo from "./bogdan-was-here.jpg";
-import tg_logo from "./Telegram-logo.svg";
-import vk_logo from "./VKcom-logo.svg";
-import tp_logo from "./Timepad_logo_75.png";
-
-function SocialButton({ text, address, children }: { text: string, address: string, children: JSX.Element }): JSX.Element {
-  return (
-    <button onClick={() => window.open(address)}
-      className={`bg-violet-800 hover:bg-violet-800/90 focus:ring-2 ring-black focus:ring-offset-2  flex items-center h-fit rounded-lg overflow-hidden p-1`}>
-      <div className="h-6 w-6 rounded-full overflow-hidden">
-        {children}
-      </div>
-      <p className="p-1 text-white text-base">{text}</p>
-    </button>
-  );
-}
+import { TimepadButton, TelegramButton, VkButton } from "../Elements/Socials";
 
 export function About(): JSX.Element {
   return (
@@ -30,15 +16,9 @@ export function About(): JSX.Element {
 
         {/* <---Socials---> */}
         <div className="flex flex-wrap gap-2 mt-2 sm:mt-4 justify-center">
-          <SocialButton text="билеты" address="https://kioskcomedy.timepad.ru">
-            <img alt="Timepad.ru logo" className="scale-110" src={tp_logo}></img>
-          </SocialButton>
-          <SocialButton text="t.me/kioskcomedy" address="https://t.me/kioskcomedy">
-            <img alt="Telegram logo" src={tg_logo}></img>
-          </SocialButton>
-          <SocialButton text="vk.com/kioskcomedy" address="https://vk.com/kioskcomedy">
-            <img alt="Vk logo" className="scale-110" src={vk_logo}></img>
-          </SocialButton>
+          <TimepadButton size="full" />
+          <TelegramButton size="full" />
+          <VkButton size="full" />
         </div>
 
         <address className="text-right text-base text-gray-500 mt-2 sm:mt-4">
