1e3af8a0c0319dbd71f2ce31c0e8eeff3a0cc069
Evgeniy Ershov <ershov.ee@gmail.com>
2023-05-31 20:50:08 +0300

    basic compression for backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
+import compression from "compression";
 
 import apiRouter from "./routes/api.routes.js";
 import "./utils/scheduler.js";
@@ -8,9 +9,19 @@ process.env.TZ = "Europe/Moscow";
 const PORT = process.env.PORT || 8080;
 const nodeEnv = process.env.NODE_ENV || "production";
 const app = express();
-app.use(express.json());
-app.use(cors());
+app.use(compression({ filter: shouldCompress }))
 
+function shouldCompress (req: Request, res: Response) {
+  if (req.headers['x-no-compression']) {
+    // don't compress responses with this request header
+    return false
+  }
+ 
+  // fallback to standard filter function
+  return compression.filter(req, res)
+}
+app.use(cors());
+app.use(express.json());
 app.use("/", apiRouter);
 
 app.get("/", (_req, res) => {
