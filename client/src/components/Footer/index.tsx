c229a3d815e8da6cb2c778b90cf8ba297a86a90b
Evgeniy Ershov <ershov.ee@gmail.com>
2023-06-01 12:53:14 +0300

    css

--- client/src/components/Footer/index.tsx
+++ client/src/components/Footer/index.tsx
@@ -4,10 +4,10 @@ import { TimepadButton, TelegramButton, VkButton } from "../Elements/Socials";
 function Footer() {
 
   return (
-    <footer className='mx-auto w-full p-4 py-3 lg:py-8'>
-      <hr className="my-4 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-      <div className="flex sm:flex sm:items-center flex-wrap justify-between">
-        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400 mb-2">2023 <a href="https://kioskcomedy.org/" className="hover:underline">kioskcomedy.org</a>
+    <footer className='mx-auto w-full p-4 py-3'>
+      <hr className="my-4 border-gray-200 sm:mx-auto dark:border-gray-700" />
+      <div className="flex items-center flex-wrap justify-between">
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400 mb-1">2023 <a href="https://kioskcomedy.org/" className="hover:underline">kioskcomedy.org</a>
         </span>
 
         {/* <---- Socials ----> */}
