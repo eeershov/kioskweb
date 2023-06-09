3515695684c470b288cebb28c6fee691eb169f27
Evgeniy Ershov <ershov.ee@gmail.com>
2023-06-09 14:46:10 +0300

    fix: og protocol does not support svg

--- client/src/components/Seo/Seo.tsx
+++ client/src/components/Seo/Seo.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import kiosklogo from "./kiosklogo.jpg";
 
 export default function Seo() {
   return (
@@ -7,7 +8,7 @@ export default function Seo() {
       <meta property="og:url" content="https://kioskcomedy.org/" />
       <meta property="og:site_name" content="KioskComedy" />
       <meta property="og:title" content="Киоск комедии" />
-      <meta property="og:image" content="https://kioskcomedy.org/kiosk.svg" />
+      <meta property="og:image" content={kiosklogo} />
       <meta property="og:image:height" content="1000" />
       <meta property="og:image:width" content="1000" />
     </Helmet>
