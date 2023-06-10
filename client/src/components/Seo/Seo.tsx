587d550840ff8490589bd2eae66aa3eb33ef1d80
root <root@prime-force.aeza.network>
2023-06-10 21:53:39 +0300

    umami analytics

--- client/src/components/Seo/Seo.tsx
+++ client/src/components/Seo/Seo.tsx
@@ -8,9 +8,10 @@ export default function Seo() {
       <meta property="og:url" content="https://kioskcomedy.org/" />
       <meta property="og:site_name" content="KioskComedy" />
       <meta property="og:title" content="Киоск комедии" />
-      <meta property="og:image" content={kiosklogo} />
+      <meta property="og:image" content={"https://kioskcomedy.org" + kiosklogo} />
       <meta property="og:image:height" content="1000" />
       <meta property="og:image:width" content="1000" />
+      <script async src="https://umami.kioskcomedy.org/script.js" data-website-id="b1beac61-5271-4b9e-ad38-aab4a087637e"></script>
     </Helmet>
   );
 }
