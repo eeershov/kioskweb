38f3b3ab701d57b467699f535a135e339762559c
Evgeniy Ershov <ershov.ee@gmail.com>
2023-06-12 13:53:17 +0300

    +helmet -cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from "express";
+import helmet from "helmet";
 import cors from "cors";
 import compression from "compression";
 
@@ -20,7 +21,8 @@ function shouldCompress(req: Request, res: Response) {
   // fallback to standard filter function
   return compression.filter(req, res);
 }
-app.use(cors());
+// app.use(cors());
+app.use(helmet());
 app.use(express.json());
 app.use("/", apiRouter);
 
