fa06886c81571e2fa7f5a2c352e6ed7449c75496
Evgeniy Ershov <38592029+eeershov@users.noreply.github.com>
2024-01-02 20:23:28 +0300

    fix: year updates

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -7,7 +7,7 @@ function Footer() {
     <footer className='mx-auto w-full p-4 py-3'>
       <hr className="my-4 border-gray-200 sm:mx-auto dark:border-gray-700" />
       <div className="flex items-center flex-wrap justify-between">
-        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400 mb-1">2024 <a href="https://kioskcomedy.org/" className="hover:underline">kioskcomedy.org</a>
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400 mb-1">{new Date().getFullYear()} <a href="https://kioskcomedy.org/" className="hover:underline">kioskcomedy.org</a>
         </span>
 
         {/* <---- Socials ----> */}
