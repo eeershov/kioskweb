a99359e8497f219460f68af51a4071dc9ac9fc6c
Evgeniy Ershov <38592029+eeershov@users.noreply.github.com>
2024-02-05 18:04:18 +0300

    refactor: style changes to accept repository pattern in Calendar

--- client/src/repositories/CalendarRepository.ts
+++ client/src/repositories/CalendarRepository.ts
@@ -2,30 +2,25 @@ import { ApiService } from "../services/api.service";
 import { EventWithOrganizationData } from "../types/EventWithOrg.type";
 import { format } from "date-fns";
 
-
 export class CalendarData {
-    data?: Map<string, [] | EventWithOrganizationData[]>;
-    errorMessage?: String;
+  data?: Map<string, [] | EventWithOrganizationData[]>;
+  errorMessage?: String;
 }
 
 export class CalendarRepository {
+  public static async fetchData(selectedDateLocal: any): Promise<CalendarData> {
+    const dateFormat = `yyyy-MM-dd`;
+    const selectedDateString = format(selectedDateLocal, dateFormat);
 
-    public static async fetchData(selectedDateLocal: any) : Promise<CalendarData> {
-        const dateFormat = `yyyy-MM-dd`;
-        const selectedDateString = format(selectedDateLocal, dateFormat);
+    const result = new CalendarData();
 
-        const result = new CalendarData()
-
-        try {
-            ApiService.getCalendarData(selectedDateString).then(response => {
-                result.data = response
-            });
-        } catch (error: any) {
-            result.errorMessage = error.message
-        }
-
-        return result
-
+    try {
+      const response = await ApiService.getCalendarData(selectedDateString);
+      result.data = response;
+    } catch (error: any) {
+      result.errorMessage = error.message;
     }
 
+    return result;
+  }
 }
